f4087c105c8cb89b02052f2db9603b869f536a8f
Josh Holland <jh36@sanger.ac.uk>
2018-08-08 15:36:20 +0100

    Add `packages` to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(
     name="cwl-dummy",
     version="0.0.1",
+    packages=["cwl_dummy"],
     python_requires=">=3.6",
     install_requires=[
         "ruamel.yaml < 0.16",
